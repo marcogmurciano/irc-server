9854789c571ce383d8963ac75e1d622b26f089a8
Marco García <marcogmurciano@gmail.com>
2025-12-12 00:00:00 +0100

    feat: Add utility classes and helper functions

---
+++ include/utils.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcoga2 <marcoga2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 15:05:14 by user1             #+#    #+#             */
+/*   Updated: 2026/02/10 15:28:44 by marcoga2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <set>
+#include <string>
+#include <vector>
+
+#define GREEN_TEXT "\033[1;32m"
+#define RESET_COLOR "\033[0m"
+
+using std::string;
+typedef std::set<string>::const_iterator setOfStringsIterator;
+typedef std::pair<setOfStringsIterator, setOfStringsIterator> pairIterators;
+
+bool ft_isLetter(char c);
+bool ft_isDigit(char c);
+bool ft_isSpecial(char c);
+string ft_itoa(int n);
+std::pair<int, bool> ft_atoi(const string& str);
+void ft_toUpper(string& str);
+void ft_toLower(string& str);
+const string& getLocalHostname();
+
+const string genSyntaxError(const string& serverName,
+    const string& nickName, const string& cmd);
+
+const string genErrUnknownCommand(const string& serverName,
+    const string& commandString);
+
+std::vector<string> split(string s, const string& delimiter);
+#endif
