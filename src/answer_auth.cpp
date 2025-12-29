7d0b00f4c67adfb31a2125418a1a7f4b73c47840
Marco García <marcogmurciano@gmail.com>
2025-12-30 00:00:00 +0100

    feat: Implement authentication handlers (PASS, NICK, USER, QUIT)

---
+++ src/answer_auth.cpp
@@ -0,0 +1,199 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   answer_auth.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcoga2 <marcoga2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 15:34:32 by mvassall          #+#    #+#             */
+/*   Updated: 2026/02/17 19:15:56 by marcoga2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "IRCChannel.hpp"
+#include "IRCClient.hpp"
+#include "IRCServ.hpp"
+#include "utils.hpp"
+#include <iostream>
+#include <sstream>
+#include <cerrno>
+#include <ctime>
+
+
+void IRCServ::answer_pass(IRCMessage& msg, int fd)
+{
+    std::ostringstream recv_pass;
+    vectorIteratorPairType range = msg.getParameters();
+    for (vector<string>::const_iterator it = range.first; it != range.second; ++it)
+	{
+        recv_pass << (it == range.first ? "" : " ") << *it;
+	}
+
+	if (recv_pass.str().empty())
+	{
+		queue_and_send(fd, "461 * PASS :Not enough parameters\r\n");
+		set_clientsToBeRemoved(fd);
+		return;
+	}
+	if (recv_pass.str() != clientPassword)
+	{
+		queue_and_send(fd, "464 * :Password incorrect\r\n");
+		set_clientsToBeRemoved(fd);
+		return;
+	}
+	clients[fd].setFlag(PASS_FLAG);
+}
+
+void IRCServ::answer_nick(IRCMessage& msg, int fd)
+{
+    if (msg.getParametersSize() == 0) {
+        queue_and_send(fd, ":server 431 :No nickname given\r\n");
+        return;
+    }
+
+    std::string new_nick = msg.getParam(0);
+    std::string old_nick = clients[fd].getNick();
+
+	if (!IRCClient::isValidNick(new_nick))
+	{
+		std::string err = ":server 432 * " + new_nick + " :Erroneous nickname\r\n";
+		queue_and_send(fd, err);
+		return;
+	}
+	if (!nickIsUnique(new_nick) && nicks[new_nick] != fd)
+	{
+		std::string err = ":server 433 * " + new_nick + " :Nickname is already in use\r\n";
+		queue_and_send(fd, err);
+		return;
+	}
+
+    clients[fd].setNick(new_nick);
+    addToNicks(new_nick, fd);
+    clients[fd].setFlag(NICK_FLAG);
+
+	if (old_nick != "")
+	{
+		rmFromNicks(old_nick);
+		std::string msg = ":" + old_nick + "!" + clients[fd].getUsername() + "@" + clients[fd].getHost() + " NICK " + new_nick + "\r\n";
+		queue_and_send(fd, msg);
+		broadcast(fd, msg, old_nick, new_nick);
+	}
+	else if (clients[fd].checkFlag(NICK_FLAG) && clients[fd].checkFlag(USER_FLAG) && clients[fd].checkFlag(PASS_FLAG)) 
+		sendWelcome(fd);
+}
+
+
+void IRCServ::answer_user(IRCMessage& msg, int fd)
+{
+    if (!clients[fd].getUsername().empty()) {
+        std::string nick = clients[fd].getNick().empty() ? "*" : clients[fd].getNick();
+        queue_and_send(fd, ":server 462 " + nick + " :Unauthorized command\r\n");
+        return;
+    }
+
+    if (msg.getParametersSize() < 4) {
+        std::string nick = clients[fd].getNick().empty() ? "*" : clients[fd].getNick();
+        queue_and_send(fd, ":server 461 " + nick + " USER :Not enough parameters\r\n");
+        return;
+    }
+
+    std::string user = msg.getParam(0);
+
+    if (!IRCClient::isValidUsername(user)) {
+        std::string nick = clients[fd].getNick().empty() ? "*" : clients[fd].getNick();
+        queue_and_send(fd, ":server 432 " + nick + " :Erroneous username\r\n");
+        return;
+    }
+
+    clients[fd].setUsername(user);
+    std::string userFullName = msg.getParam(3);
+    clients[fd].setFullname(userFullName);
+    clients[fd].setFlag(USER_FLAG);
+
+    if (clients[fd].checkFlag(NICK_FLAG) && clients[fd].checkFlag(USER_FLAG) && clients[fd].checkFlag(PASS_FLAG))
+        sendWelcome(fd);
+}
+
+void IRCServ::queue_and_send(int fd, std::string data)
+{
+    clients[fd].addToObuffer(data);
+
+    std::string& buffer = clients[fd].getObuffer();
+
+    ssize_t sent = send(fd, buffer.c_str(), buffer.size(), MSG_NOSIGNAL);
+    if (sent > 0)
+        buffer.erase(0, sent);
+
+		else if (sent == -1) 
+		{
+    	if (errno != EAGAIN && errno != EWOULDBLOCK)
+			{
+        this->set_clientsToBeRemoved(fd); 
+        return;
+			}
+		}
+		
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = EPOLLIN | EPOLLET;
+    if (!buffer.empty())
+        ev.events |= EPOLLOUT;
+
+    epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &ev);
+}
+
+void IRCServ::broadcast(int fd, std::string notify_msg, string oldnick, string newnick)
+{
+    std::set<int> targets;
+    targets.insert(fd);
+
+	if (clients.count(fd) == 0) return;
+	IRCClient client = clients[fd];
+	pairIterators channelIterators = clients[fd].getChannelIterators();
+	for (setOfStringsIterator chNameIt = channelIterators.first;
+		chNameIt != channelIterators.second; ++chNameIt) {
+		string channelName = *chNameIt;
+		if (channels.count(channelName) == 0) continue;
+		PairUserMapIterators uIts = channels[channelName].getUsersIterators();
+		
+		for (map<string,UserMode>::const_iterator uit=uIts.first; uit != uIts.second; ++uit)
+		{
+				string nickName = uit->first;
+				if (nicks.count(nickName) == 0) continue;
+				targets.insert(nicks[nickName]);
+		}
+		
+		if (oldnick != "" && newnick != "")
+		{
+			channels[channelName].delUser(oldnick);
+			channels[channelName].addUser(newnick);
+		}
+	}
+	for (std::set<int>::iterator it = targets.begin(); it != targets.end(); ++it)
+		queue_and_send(*it, notify_msg);
+}
+
+void IRCServ::sendWelcome(int fd)
+{
+    std::string nick = clients[fd].getNick();
+    std::string user = clients[fd].getUsername();
+    std::string host = clients[fd].getHost();
+    std::string sName = getServerName();
+
+    std::string rpl001 = ":" + sName + " 001 " + nick + " :Welcome to the Internet Relay Network " + nick + "!" + user + "@" + host + "\r\n";
+    queue_and_send(fd, rpl001);
+
+    std::string rpl002 = ":" + sName + " 002 " + nick + " :Your host is " + sName + ", running version 1.0\r\n";
+    queue_and_send(fd, rpl002);
+
+		time_t t = time(0);
+		char b[32];
+		std::string rpl003 = ":" + sName + " 003 " + nick + " :You joined this server at " + (strftime(b, 32, "%a %b %d %Y", localtime(&t)), b) + "\r\n";
+    queue_and_send(fd, rpl003);
+
+    std::string rpl004 = ":" + sName + " 004 " + nick + " " + sName + " 1.0 i ntklo\r\n";
+    queue_and_send(fd, rpl004);
+
+    std::string rpl376 = ":" + sName + " 376 " + nick + " :End of /MOTD command.\r\n";
+    queue_and_send(fd, rpl376);
+}
